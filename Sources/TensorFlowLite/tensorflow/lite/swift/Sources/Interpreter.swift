649095f846ae2d962a70a1c9e3c0aeaac9998195
KIM BONGMO <bongmo.kim@sk.com>
2022-09-21 20:02:53 +0900

    - code update : 2.9.0

--- Sources/TensorFlowLite/tensorflow/lite/swift/Sources/Interpreter.swift
+++ Sources/TensorFlowLite/tensorflow/lite/swift/Sources/Interpreter.swift
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 import Foundation
 import TensorFlowLiteC
 
@@ -314,7 +313,6 @@ public final class Interpreter {
   }
 
   // MARK: - Private
-
   private func configureXNNPack(options: Options, cInterpreterOptions: OpaquePointer) {
     var cXNNPackOptions = TfLiteXNNPackDelegateOptionsDefault()
     if let threadCount = options.threadCount, threadCount > 0 {
